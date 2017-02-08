ede30209b2c9b78ec7e631cc0c7ad4d5cb55dfa7
bryanrtboy <bryan@bryanleister.com>
2017-02-07 19:36:17 -0700

    Update to run on a new thread
    We still fetch XML in the most simple way for current weather, but for
    a slower/larger feed like a weather forecast we open a new thread to
    remove a stall on the main Update() thread.

---
+++ Assets/XMLReader/Scripts/ShowUI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SimpleXML;
+
+public class ShowUI : MonoBehaviour
+{
+
+	public Text m_current;
+	public Text m_forecast;
+	public XMLReader m_xmlReader;
+
+
+	void OnEnable ()
+	{
+		m_xmlReader.OnForecastReceived += ReadForecast;
+		m_xmlReader.OnCurrentCondReceived += ReadCurrent;
+	}
+
+	void OnDisable ()
+	{
+		m_xmlReader.OnForecastReceived -= ReadForecast;
+		m_xmlReader.OnCurrentCondReceived -= ReadCurrent;
+	}
+
+	void ReadForecast (string s, float f)
+	{
+		if (m_forecast != null)
+			m_forecast.text = "Forecast is " + s + ", with a temperature of " + f.ToString () + " at " + Time.time + " after start.";
+	}
+
+	void ReadCurrent (string s, float f)
+	{
+		if (m_current != null)
+			m_current.text = "Currently it is " + s + ", with a temperature of " + f.ToString () + " at " + Time.time + " after start.";
+	}
+}
--- Assets/XMLReader/Scripts/XMLReader.cs
+++ Assets/XMLReader/Scripts/XMLReader.cs
@@ -6,38 +6,185 @@
 //
 //Some feeds do not require that you send a header, but the us weather does, so it includes that
 //function
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
+using System.Threading;
 using System.Xml;
+using System.Xml.Linq;
 using System.IO;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace SimpleXML
 {
 	public class XMLReader : MonoBehaviour
 	{
-		public Text m_display;
-		public string m_feedURL = "http://w1.weather.gov/xml/current_obs/KFTG.xml";
+
+		public event Action<string,float> OnCurrentCondReceived;
+		public event Action<string,float> OnForecastReceived;
+
 		//other feeds to test -  http://feeds.foxnews.com/foxnews/national
-		//http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml";
+		//string m_forecastURL = "http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml";
+		string m_forecastURL = "http://forecast.weather.gov/MapClick.php?lat=39.7392&lon=-104.9903&unit=0&lg=english&FcstType=dwml";
+		string m_currentWeatherURL = "http://w1.weather.gov/xml/current_obs/KFTG.xml";
+		string m_userAgent = "MyApplication/v1.0 (http://foo.com; foo@gmail.com)";
+
+		Thread m_thread;
+		XDocument xmlDoc;
+		bool runThread = true;
+		bool updateThread = false;
+		bool haveFreshData = false;
 
 		void Start ()
 		{
-			SendRequest ();
+			InvokeRepeating ("GetCurrentConditions", 0, 15);//this runs on the main thread, which is fine since this feed is fast
+			InvokeRepeating ("GetForecast", 1, 36); //forecast only updates once per hour so use 3600 here. It will run on it's own thread as reading is slower...
 		}
 
-		void SendRequest ()
+		void Update ()
 		{
-			XmlDocument doc = new XmlDocument ();
+			if (haveFreshData) { //Don't do anything unless the thread has refreshed the data
+				haveFreshData = false; //then we only run once
 
-			string stationName = m_feedURL;
+				List<string> tempList = Temperatures (xmlDoc);
+				List<string> condList = Conditions (xmlDoc);
 
-			HttpWebRequest request = (HttpWebRequest)WebRequest.Create (stationName);
+				if (tempList.Count > 0 && condList.Count > 0) {
+					if (OnForecastReceived != null)
+						OnForecastReceived (condList [0], Convert.ToSingle (tempList [0]));
+				}
+			}
+		}
+
+		void OnEnable ()
+		{
+			m_thread = new Thread (ThreadUpdate); //starting the thread that reads forecasts, since it is slow...
+			m_thread.Start (); //Thread runs all the time, but only does anything when updateThread = true;
+		}
+
+		void OnDisable ()
+		{
+			EndThreads ();
+		}
+
+		void EndThreads ()
+		{
+			runThread = false;
+			//you could use thread.abort() but that has issues on iOS
+			while (m_thread.IsAlive) {
+				//simply have main loop wait till thread ends
+			}
+		}
+
+		#region Forecast reading using XDocument running on it's own thread
+
+		void ThreadUpdate ()
+		{
+			while (runThread) {
+				if (updateThread) {
+					updateThread = false;
+					FetchForecastXDocument (m_forecastURL);
+				}
+			}
+		}
+
+		void GetForecast ()
+		{
+			updateThread = true;
+		}
+
+		List<string> Temperatures (XDocument xml)
+		{
+			var maximums = from tempvalue in xml.Descendants ("temperature").Elements ("value")
+			               where tempvalue.Parent.Attribute ("type").Value == "maximum"
+			               select (string)tempvalue;
+
+			List<string> returnme = maximums.ToList<string> ();
+			return returnme;
+		}
+
+		List<string> Conditions (XDocument xml)
+		{
+			List<string> returnme = new List<string> ();
+
+			foreach (XAttribute x in xml.Descendants ("weather").Elements("weather-conditions").Attributes("weather-summary"))
+				returnme.Add (x.Value);
+			return returnme;
+			
+		}
+
+		void FetchForecastXDocument (string url)
+		{
+			xmlDoc = GetXDocument (url);
+			haveFreshData = true;
+		}
+
+		XDocument  GetXDocument (string xmlURL)
+		{
+			XDocument doc = new XDocument ();
+			float elapsedTime = 0.0f;
+
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create (xmlURL);
 
 			request.Method = "GET";
 			request.ContentType = "application/x-www-form-urlencoded";
-			request.UserAgent = "MyApplication/v1.0 (http://foo.com; foo@gmail.com)";
+			request.UserAgent = m_userAgent;
+			request.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+
+			HttpWebResponse response = (HttpWebResponse)request.GetResponse ();
+			Stream resStream = response.GetResponseStream ();
+
+			doc = XDocument.Load (resStream);
+
+			return doc;
+		}
+
+		#endregion
+
+		#region Current Conditions using XmlDocument
+
+		void GetCurrentConditions ()
+		{
+			XmlDocument doc = GetXMLDocument (m_currentWeatherURL);
+
+			float temp = -100f;
+			string weather = "";
+
+			try {
+
+				XmlNodeList newList = doc.DocumentElement.ChildNodes;
+
+				foreach (XmlNode n in newList) {
+					Debug.Log (n.OuterXml);
+
+					if (n.Name == "weather")
+						weather = n.InnerText;
+
+					if (n.Name == "temp_f")
+						temp = Convert.ToSingle (n.InnerText);
+					
+				}
+
+				if (OnCurrentCondReceived != null)
+					OnCurrentCondReceived (weather, temp);
+
+			} catch (System.Exception e) {
+				Debug.LogError ("Unknown exception: " + e.Message + " " + e.StackTrace);
+			}
+
+		}
+
+		XmlDocument GetXMLDocument (string xmlURL)
+		{
+			XmlDocument doc = new XmlDocument ();
+
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create (xmlURL);
+
+			request.Method = "GET";
+			request.ContentType = "application/x-www-form-urlencoded";
+			request.UserAgent = m_userAgent;
 			request.Accept = "Accept=text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
 
 			HttpWebResponse response = (HttpWebResponse)request.GetResponse ();
@@ -45,20 +192,10 @@ namespace SimpleXML
 
 			doc.Load (resStream);
 
-			try {
-				XmlNodeList newList = doc.DocumentElement.ChildNodes;
-
-				foreach (XmlNode n in newList) {
-					Debug.Log (n.OuterXml);
-
-					if (n.Name == "weather" && m_display != null)
-						m_display.text = n.InnerText;
-				}
-
-			} catch (System.Exception e) {
-				Debug.LogError ("Unknown exception: " + e.Message + " " + e.StackTrace);
-			}
-
+			return doc;
 		}
+
+		#endregion
+
 	}
 }
